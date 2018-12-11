b35757b20807b0d0aa766d909c3bac98d45f8fd3
Anandalopes8 <ananda.lopes.8@gmail.com>
2018-12-11 11:05:36 -0300

    inicio

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+
+require_once("vendor/autoload.php");
+
+$app = new \Slim\Slim();
+
+$app->config('debug', true);
+
+$app->get('/', function() {
+    
+	$sql = new Hcode\DB\Sql();
+	$result = $sql->select("SELECT * FROM tb_user");
+	echo json_encode($result);
+
+});
+
+$app->run();
+
+ ?>
